c39aa73f9351149ddaaf850fad256f54c0f40730
nmdgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2020-06-04 21:26:53 +0200

    Clean up code

--- app/calls.py
+++ app/calls.py
@@ -1,4 +1,5 @@
 import pyowm
+import requests
 
 def get_current_weather():
     owm = pyowm.OWM('f12ef8a8dc87e23556868b9967690c88')
@@ -6,3 +7,9 @@ def get_current_weather():
     weather = observation.get_weather()
     temperature = weather.get_temperature('celsius')['temp']
     return str(round(temperature))
+
+def get_cat_fact():
+    response = requests.get("https://meowfacts.herokuapp.com/")
+    response = response.json()['data']
+    fact = ''.join(map(str, response))
+    return fact
--- app/main.py
+++ app/main.py
@@ -9,56 +9,56 @@ import random
 import json
 import pickle
 
-import weather
+import calls
 
 with open("intents.json") as file:
     data = json.load(file)
 
-# try:
-#     with open("data.pickle", "rb") as f:
-#         words, labels, training, output = pickle.load((f))
-# except:
-words = []
-labels = []
-docs_x = []
-docs_y = []
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load((f))
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-labels = sorted(labels)
+    labels = sorted(labels)
 
-training = []
-output = []
+    training = []
+    output = []
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-    wrds = [stemmer.stem(w) for w in doc]
+        wrds = [stemmer.stem(w) for w in doc]
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
 training = numpy.array(training)
 output = numpy.array(output)
@@ -77,11 +77,11 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-# try:
-#     model.load("model.tflearn")
-# except:
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -97,25 +97,20 @@ def bag_of_words(s, words):
     return numpy.array(bag)
 
 def chat(inp):
-    #print("Say hello to the bot! (type quit to stop)")
-
     while True:
-        #inp = input("You: ")
-
-        if inp.lower() == "quit":
-            break
-
         results = model.predict([bag_of_words(inp, words)])
         results_index = numpy.argmax(results)
         tag = labels[results_index]
+
         
         for tg in data["intents"]:
             if tg["tag"] == tag:
                 responses = tg["responses"]
 
         if tag == "weather":
-            return random.choice(responses) + weather.get_current_weather() + " °C"
+            return random.choice(responses) + calls.get_current_weather() + " °C"
+        
+        if tag == "cats":
+            return random.choice(responses) + calls.get_cat_fact()
 
         return random.choice(responses)
-
-#chat()
